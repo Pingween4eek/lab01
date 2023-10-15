32796ad1544e2220105625e63ed36ff82dd5e1cd
Ivan Garkalin <ivan.garkalin2005@mail.ru>
2023-10-15 18:04:53 +0300

    Change code in prog.c

--- prog.c
+++ prog.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-int x, y, z;
+double x, y, u;
+double max_x, max_y, max_u = -10.0;
 
-for (x = 1; x <= 5; x++)
-for (y = 2; y <= 6; y += 2){
-z = 2 * x + 3 * y;
-printf("при x=%d, при y=%d, z=%d\n", x, y, z);
+for (x = 0.0; x <= 2.0; x += 0.3){
+for (y = 0.0; y <= 1.0; y += 0.4){
+u = 4 * cos(cbrt(y)) * sin(x * fabs(y * y * y - 1.4));
+if (u > max_u){
+max_u = u;
+max_x = x;
+max_y = y;
+} 
+}
+printf("x: %lf, y: %lf, u: %lf", x, y, u); 
 }
 
 return 0;
